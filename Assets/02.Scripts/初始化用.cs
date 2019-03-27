0555e2dc0fd9f766ad54ad4ac1757dd1658b66c3
皓翔 张 <wszhhx@163.com>
2019-03-27 16:54:04 +0800

    划分目录结构

---
+++ Assets/02.Scripts/初始化用.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 初始化用 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("TEST");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
